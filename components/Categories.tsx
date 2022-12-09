f60a577fcd478f5f5a7131eeb089a7b99ba40ffa
Jiří Šuster <suster.ji@gmail.com>
2022-12-09 21:22:51 +0100

    Categories fetch

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import CategoryCard from './CategoryCard'
 
 function Categories() {
+    const [categoriesArray, setCategories] = useState([])
+
+    useEffect(() => {
+        axios.get('https://api.chucknorris.io/jokes/categories')
+        .then(res => {
+            setCategories(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        }
+        )
+    }, []) 
+
+    interface Category {
+        name: string
+    }
+
+    let categories: Category[] = [];
+
+    categoriesArray.forEach(element => {
+        const objekt:Category = {
+            name: element
+        }
+        categories.push(objekt)
+    });
+    
   return (
     <div className="bg-gray-900 h-screen w-64 m-0 fixed top-4 left-4 text-white">
         <div className="pb-4 pt-4 text-center">Joke Categories</div>
-        <CategoryCard name='Mother' />
-        <CategoryCard name='Dad' />
-        <CategoryCard name='Food' />
+        {
+            categories.map(category => <CategoryCard key={category.name} name={category.name}/>)
+        }
     </div>   
   )
 }
