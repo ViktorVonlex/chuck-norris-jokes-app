267e042baca73274a57e30859c4ae44213e8bbfb
Jiří Šuster <suster.ji@gmail.com>
2022-12-10 15:32:53 +0100

    Made a bit more responsive and beautiful

--- components/MiddlePanel.tsx
+++ components/MiddlePanel.tsx
@@ -10,9 +10,9 @@ export default function MiddlePanel({}: Props) {
             <div className="w-2/3 mr-1 middle-button p-4">Get random joke!</div>
             <div className="w-1/3 ml-1 middle-button p-4">Save this joke</div>
         </div>
-        <div className="w-full bg-gray-900 mt-5 p-4 rounded-3xl">
+        <div className="w-full bg-gray-900 mt-5 pt-4 rounded-3xl">
             <div className="text-center text-white text-5xl mb-5">The Joke</div>
-            <div className="text-center bg-gray-800 w-full text-green-600 text-2xl rounded-3xl p-2">Chuck Norris's football team won the NBA champinship against the tennis player Roger Federer</div>
+            <div className="text-center bg-gray-800 w-full text-green-600 text-2xl rounded-b-3xl p-4 pb-10 pt-10">Chuck Norris's football team won the NBA champinship against the tennis player Roger Federer</div>
         </div>
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ export default function Home() {
     <div className="flex mx-auto px-4 bg-gray-400 h-screen">
       <Categories />
       <MiddlePanel />
+      <div className="w-1/6 bg-gray-900 mt-5">dasdasda</div>
     </div>
     </>
   )
