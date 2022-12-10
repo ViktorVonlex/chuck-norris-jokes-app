3597d43b9917313f9e0857f6aafc7fec83aeb5c8
Jiří Šuster <suster.ji@gmail.com>
2022-12-10 11:11:40 +0100

    Fixed fetch

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,39 +1,28 @@
-import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import CategoryCard from './CategoryCard'
 
 function Categories() {
-    const [categoriesArray, setCategories] = useState([])
+    
+    const [categoriesArray, setCategories] = useState<string[]>([])
 
     useEffect(() => {
-        axios.get('https://api.chucknorris.io/jokes/categories')
-        .then(res => {
-            setCategories(res.data)
+        fetch('https://api.chucknorris.io/jokes/categories')
+        .then(res =>  res.json()
+        )
+        .then(data => {
+            setCategories(data)
         })
         .catch(err => {
             console.log(err)
         }
         )
-    }, []) 
-
-    interface Category {
-        name: string
-    }
-
-    let categories: Category[] = [];
-
-    categoriesArray.forEach(element => {
-        const objekt:Category = {
-            name: element
-        }
-        categories.push(objekt)
-    });
+    }, [])    
     
   return (
     <div className="bg-gray-900 h-screen w-64 m-0 fixed top-4 left-4 text-white">
         <div className="pb-4 pt-4 text-center">Joke Categories</div>
         {
-            categories.map(category => <CategoryCard key={category.name} name={category.name}/>)
+            categoriesArray.map(category => <CategoryCard key={category} name={category}/>)
         }
     </div>   
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Categories from "../components/Categories";
 export default function Home() {
   return (
     <>
-    <div className="flex mx-auto px-4 h-screen">
+    <div className="flex mx-auto px-4 bg-gray-400 h-screen">
       <Categories />
     </div>
     </>
