06cfa376c7ade6f1c8e196135eaefa4b1325fe8a
ViktorVonlex <suster.ji@gmail.com>
2023-01-10 21:42:16 +0100

    Login design

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { saveUser } from '../utils/utils';
 
 type Props = {}
 
 function Login({}: Props) {
+
+    const [userInfo, setUserInfo] = useState({ email: "", password: "" });
 
     function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         // Prevent the browser from reloading the page
@@ -20,13 +22,33 @@ function Login({}: Props) {
       }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username: <input name="myInput" className="text-green-600" defaultValue="Enter your username" />
-      </label>
-      <button type="reset">Reset form</button>
-      <button type="submit">Submit form</button>
-    </form>
+    <div className="mx-auto w-5/6">
+        <form onSubmit={handleSubmit} className="flex flex-col justify-center">
+            <input
+                className="rounded-2xl mx-2 text-center text-green-600 h-9"
+                value={userInfo.email}
+                onChange={({ target }) =>
+                    setUserInfo({ ...userInfo, email: target.value })
+                }
+                type="email"
+                placeholder="john@email.com"
+                />
+                <input
+                className="rounded-2xl m-2 mb-3 mt-3 text-center text-green-600 h-9"
+                value={userInfo.password}
+                onChange={({ target }) =>
+                    setUserInfo({ ...userInfo, password: target.value })
+                }
+                type="password"
+                placeholder="********"
+                />
+        </form>
+        <div className="flex flex-col justify-center">
+            <button type="reset" className="category-entry">Login</button>
+            <button type="submit" className="category-entry">Register</button>   
+        </div>
+    </div>
+    
   )
 }
 
