69b46be6188a8a50420616445d791358867f49b2
Jiří Šuster <suster.ji@gmail.com>
2023-01-31 09:53:58 +0100

    Added serverSideProps and some SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import { Item } from "../utils/utils";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import Loading from "../components/Loading";
+import Head from "next/head";
+
 
 const FavJokes = dynamic(() => import('../components/FavJokes'), {
   ssr: false,
@@ -14,7 +16,17 @@ const MiddlePanel = dynamic(() => import('../components/MiddlePanel'), {
   ssr: false,
 })
 
-export default function Home() {
+type Props = {
+  data: {
+    jokeCategories: string[],
+    randomJoke : {
+      value: string,
+      url: string
+    }
+  }
+}
+
+export default function Home({data}: Props) {
 
   const [categoriesArray, setCategories] = useState<string[]>([]);
   const [joke, setJoke] = useState<string>("");
@@ -67,22 +79,21 @@ export default function Home() {
     }
 
     useEffect(() => {
-      fetch('https://api.chucknorris.io/jokes/categories')
-      .then(res =>  res.json()
-      )
-      .then(data => {
-          setCategories(data)
-      })
-      .catch(err => {
-          console.log(err)
-      }
-      )
-      getRandomJoke()
+      setCategories(data.jokeCategories)
+      setJoke(data.randomJoke.value);
+      setJokeUrl(data.randomJoke.url);
       setLoading(false)        
   }, [])
 
   return (
     <>
+    <Head>
+      <title>Chuck Norris Jokes App</title>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta name="description" content="App for funny jokes" />
+      <meta name="keywords" content="Chuck Norris" />
+    </Head>
     <div className="flex mx-auto px-4 bg-gray-400 h-screen">
       <Categories categoriesArray={categoriesArray} getJokeFromCategory={getJokeFromCategory}/>
       <MiddlePanel joke={joke} jokeUrl={jokeUrl} getRandomJoke={getRandomJoke} setLoading={setLoading} setFetchedNew={setFetchedNew}/>
@@ -103,4 +114,18 @@ export default function Home() {
     </div>
     </>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch('https://api.chucknorris.io/jokes/categories')
+  const jokeCategories = await res.json()
+  const res2 = await fetch('https://api.chucknorris.io/jokes/random')
+  const randomJoke = await res2.json()
+    
+  return {
+    props: {data: {
+      jokeCategories,
+      randomJoke
+    }}, // will be passed to the page component as props
+  }
 }
